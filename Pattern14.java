df8bf9b427b297c8458315168fbca364999380b0
PrajwalKathole <142924084+PrajwalKathole@users.noreply.github.com>
2025-04-15 17:14:47 +0530

    Add files via upload

---
+++ Pattern14.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Pattern14 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        for (int i=1 ;i<=10;i++){
+            int v = x * i;
+            System.out.println(x+" * "+i+" = "+v);
+        }
+    }
+}
